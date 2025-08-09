0c9ee3bf126456088f1426e0c49a678e6a722ce9
EAL <elias@takefive.dk>
2025-08-09 20:27:15 +0200

    added more walls

--- src/Util/LevelCreator.cpp
+++ src/Util/LevelCreator.cpp
@@ -28,6 +28,21 @@ void LevelCreator::createLevel(EngineBase* engineBase, GameState* gameState)
   gameState->walls_.push_back(rightWall);
   engineBase->registerDrawAble(rightWall->drawAble_);
   rightWall->drawAble_->setPosition(1200 - edgeWallWidth, 0);
+
+  auto leftTargetWall = std::make_shared<Wall>(edgeWallWidth, 200);
+  leftTargetWall->drawAble_->setPosition(250, 300);
+  engineBase->registerDrawAble(leftTargetWall->drawAble_);
+  gameState->walls_.push_back(leftTargetWall);
+
+  auto rightTargetWall = std::make_shared<Wall>(edgeWallWidth, 200);
+  rightTargetWall->drawAble_->setPosition(1200 - 250 - edgeWallWidth, 300);
+  engineBase->registerDrawAble(rightTargetWall->drawAble_);
+  gameState->walls_.push_back(rightTargetWall);
+
+  auto middleWall = std::make_shared<Wall>(300, edgeWallWidth);
+  middleWall->drawAble_->setPosition(600 - (middleWall->drawAble_->getWidth() / 2), 400 - edgeWallWidth / 2);
+  engineBase->registerDrawAble(middleWall->drawAble_);
+  gameState->walls_.push_back(middleWall);
 }
 
 void LevelCreator::addPlayer(EngineBase* engineBase, GameState* gameState)
@@ -42,7 +57,7 @@ void LevelCreator::addPlayer(EngineBase* engineBase, GameState* gameState)
   }
   else if (playerCount_ == 1)
   {
-    character->drawAble_->setPosition(800, 350);
+    character->drawAble_->setPosition(1000, 350);
   }
   playerCount_++;
 }
